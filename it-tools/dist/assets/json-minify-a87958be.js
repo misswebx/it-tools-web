49e33ef612c33bc6932feab5ce3ed847d6dbc7ef
dev <dev@dev.com>
2026-03-26 03:44:46 +0800

    add

--- it-tools/dist/assets/json-minify-a87958be.js
+++
@@ -1,5 +0,0 @@
-import{_ as a}from"./FormatTransformer.vue_vue_type_script_setup_true_lang-d5b2548c.js";import{l as r}from"./index-231c3e11.js";import{w as i}from"./defaults-4d6daddf.js";import{d as s,o as l,c as p}from"./index-c4aaf8c0.js";import"./TextareaCopyable-a26043a5.js";import"./Copy-1b2496a3.js";import"./Scrollbar-0223f5ca.js";const u=`{
-	"hello": [
-		"world"
-	]
-}`,N=s({__name:"json-minify",setup(m){const t=o=>i(()=>JSON.stringify(r.parse(o),null,0),""),e=[{validator:o=>o===""||r.parse(o),message:"Provided JSON is not valid."}];return(o,f)=>{const n=a;return l(),p(n,{"input-label":"Your raw JSON","input-default":u,"input-placeholder":"Paste your raw JSON here...","output-label":"Minified version of your JSON","output-language":"json","input-validation-rules":e,transformer:t})}}});export{N as default};
